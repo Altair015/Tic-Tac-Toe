33ccb43f60c989608d2a12b50ed9dc9ab4a1dd70
Altair015 <mand23sngh@gmail.com>
2024-04-04 13:04:00 +0530

    Build the game logic and tested with the possible inputs. Game seems to work fine.

---
+++ index.js
@@ -0,0 +1,13 @@
+const boxes = document.querySelectorAll("box");
+const winPattern = [
+    [{ 0}, 1, 2],
+    [0, 3, 6],
+    [0, 4, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+]
+
+
--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const boxes = document.getElementsByClassName("box");
 const resetButton = document.getElementById("reset-button");
-console.log(boxes)
+const statusDisplay = document.getElementById("display-item");
+// console.log(statusDisplay.textContent)
 const winPattern = [
     [0, 1, 2],
     [0, 3, 6],
-    [0, 4, 9],
+    [0, 4, 8],
     [1, 4, 7],
     [2, 5, 8],
     [2, 4, 6],
@@ -13,40 +14,90 @@ const winPattern = [
 ]
 
 let playerturn = true;
-
 let count = 0;
+let winner = "";
 
-[...boxes].forEach((box, index) => {
+onLod = true;
+if (onLod) {
+    statusDisplay.style.fontSize = "xx-large";
+    statusDisplay.textContent = `Welcome to Tic Tac Toe`
+    statusDisplay.style.fontWeight = "bold";
+    setTimeout(() => {
+        statusDisplay.style.fontWeight = "";
+        statusDisplay.style.fontSize = "xxx-large";
+        statusDisplay.textContent = "O's Turn."
+        onLod = false;
+    }, 2000);
+}
+
+
+[...boxes].forEach((box) => {
     box.addEventListener("click", () => {
-        if (box.textContent == "") {
-            if (playerturn) {
-                box.textContent = "O"
-                playerturn = false;
+        setTimeout(() => {
+            checkWinner()
+        }, 300);
+        if (winner == "") {
+            if (box.textContent == "") {
+                if (playerturn) {
+                    box.textContent = "O"
+                    playerturn = false;
+                    box.style.color = "ivory";
+                    statusDisplay.textContent = "X's Turn.";
+                    count++;
+                }
+                else {
+                    box.textContent = "X"
+                    playerturn = true;
+                    statusDisplay.textContent = "O's Turn.";
+                    count++;
+                }
+
             }
-            else {
-                box.textContent = "X"
-                playerturn = true;
-            }
-            count++
         }
+
     })
 });
 
 resetButton.addEventListener("click", () => {
     [...boxes].forEach((box) => {
         box.textContent = "";
+        winner = ""
+        count = 0;
+        statusDisplay.style.fontSize = "xxx-large";
+        statusDisplay.style.fontWeight = "";
+        statusDisplay.textContent = "";
+        winPattern.forEach(
+            (pattern) => {
+                boxes[pattern[0]].style.color = "royalblue";
+                boxes[pattern[1]].style.color = "royalblue";
+                boxes[pattern[2]].style.color = "royalblue";
+            }
+        )
     })
 })
 
-// boxes[0].addEventListener("click", () => {
-//     if (playerturn) {
-//         boxes[0].textContent = "O"
-//         playerturn = false;
-//     }
-//     else {
-//         boxes[0].textContent = "X"
-//         playerturn = true;
-//     }
-//     boxes[0].disabled = true;
-//     count++
-// })
+function checkWinner() {
+    winPattern.forEach(
+        (pattern) => {
+            const firstBoxValue = boxes[pattern[0]].textContent;
+            const secondBoxValue = boxes[pattern[1]].textContent;
+            const thirdBoxValue = boxes[pattern[2]].textContent;
+            if (firstBoxValue != "" && secondBoxValue != "" && thirdBoxValue != "") {
+                if (firstBoxValue == secondBoxValue && secondBoxValue == thirdBoxValue) {
+                    winner = firstBoxValue;
+                    boxes[pattern[0]].style.color = "red";
+                    boxes[pattern[1]].style.color = "red";
+                    boxes[pattern[2]].style.color = "red";
+                    statusDisplay.style.fontSize = "xx-large";
+                    statusDisplay.style.fontWeight = "bold";
+                    statusDisplay.textContent = `${winner} has won the game.`
+                }
+            }
+            if (count == 9 && winner == "") {
+                statusDisplay.style.fontSize = "xx-large";
+                statusDisplay.style.fontWeight = "bold";
+                statusDisplay.textContent = `Match is drawn.`
+            }
+        }
+    )
+}
